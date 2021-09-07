048543a1af9e8b1e9995618525fb047d2afc16d6
Nikhil More <nikhilmore457@gmail.com>
2021-09-07 13:38:33 +0530

    first commit

---
+++ src/main/java/com/spring/springbootrun/HelloController.java
@@ -0,0 +1,12 @@
+package com.spring.springbootrun;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController
+{
+@RequestMapping("/")
+public String hello() 
+{
+return "Hello User";
+}
+}
